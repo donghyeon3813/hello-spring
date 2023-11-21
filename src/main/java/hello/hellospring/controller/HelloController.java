8b152fbce6bec21254be205bf22feab21a528a33
donghyun3813 <ehdgusdl5753@gmail.com>
2023-11-21 22:59:53 +0900

    MVC, 템플릿 엔진, API 공부

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,44 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    /*
+    * HttpMessageConverter가 동작
+    * 기본 문자 StringHttpMessageConverter가 동작
+    * 기본 객체 MappingJackson2HttpMessageConverter가 동작
+    * byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+    * */
+    @GetMapping("hello-api")
+    @ResponseBody //객체를 반환하면 json 방식으로 http 응답에 반환 (기본정책)
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+
+            return name;
+        }
+
+        public void setName(String name) {
+
+            this.name = name;
+        }
     }
 }
