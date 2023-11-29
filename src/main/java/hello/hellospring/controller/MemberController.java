045bb5e23f07592de59c76de91d97cc104731639
donghyun3813 <ehdgusdl5753@gmail.com>
2023-11-29 22:53:47 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller// 컨트롤러를 선언하면 스프링이 처음 시작될 때 스프링 컨테이너에서 MemberController를 관리한다.
 public class MemberController {
@@ -19,5 +22,20 @@ public class MemberController {
     @Autowired //스프링 컨테이너에서 관리되는 인스턴스를 가져와준다. DI
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
