9914884e5e0d3c41b9728952a2b542cebd82e748
donghyun3813 <ehdgusdl5753@gmail.com>
2023-12-14 22:20:01 +0900

    Spring Data Jpa 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -16,11 +16,17 @@ import javax.sql.DataSource;
 @Configuration //bean 객체를 직접 관리 하기 위한 설정
 public class SpringConfig {
 
-    private EntityManager em;
+//    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
     //    private DataSource dataSource;
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
@@ -29,15 +35,16 @@ public class SpringConfig {
 
     @Bean //@Bean을 통해 스프링컨테이너에 등록해준다.
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository(); 스프링의 di를 이용하면 기존 코드를 손대지 않고, 설정만으로 구현 클래스를 변경할 수 있다.
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberREpository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+
+//    }
 
 }
